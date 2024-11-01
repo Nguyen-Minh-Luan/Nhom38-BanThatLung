eb6f337538fe2da55f1b6ce6ffe4ab1a4d1a00f2
HuynhMinhThangV <22130251@st.hcmuaf.edu.vn>
2024-11-01 13:59:26 +0700

    handle payment method

--- cartPage/cartPage.js
+++ cartPage/cartPage.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
   $(".cart__container--coupon").click(function () {
-    $(this).next(".cart__container_coupon--submit").toggleClass("active");
+    $(this).next(".cart__container_coupon--submit").slideToggle();
   });
 });
--- cartPage/checkoutPage/checkout.js
+++ cartPage/checkoutPage/checkout.js
@@ -1,27 +1,117 @@
 $(document).ready(function () {
-  $(".cart__container--coupon").click(function () {
-    $(this).next(".cart__container_coupon--submit").slideToggle();
-  });
-  $("#openFormButton").click(function () {
-    $("#popupForm").fadeIn(); // Show the popup
-  });
+  const popUpUpdateInfo = () =>
+    function () {
+      const name__field1 = $(this)
+        .closest(".popup--content--userInfo--action")
+        .find(".name__field1")
+        .text();
+      const address__field1 = $(this)
+        .closest(".popup--content--userInfo--action")
+        .find(".address__field1")
+        .text();
+      const city__field1 = $(this)
+        .closest(".popup--content--userInfo--action")
+        .find(".city__field1")
+        .text();
+      const number__field1 = $(this)
+        .closest(".popup--content--userInfo--action")
+        .find(".number__field1")
+        .text();
 
-  $("#closeFormButton").click(function () {
-    $("#popupForm").fadeOut(); // Hide the popup
-  });
+      console.log(name__field1, address__field1, city__field1, number__field1);
 
-  $("#submitButton").click(function () {
-    const newText = $("#newText").val();
-    if (newText) {
-      $("#displayText").text(newText); // Update the text
-      $("#popupForm").fadeOut(); // Hide the popup
-      $("#newText").val(""); // Clear input field
-    } else {
-      alert("Please enter some text"); // Alert if input is empty
+      if (name__field1 && address__field1 && city__field1 && number__field1) {
+        $(".userInfo__name").text(name__field1);
+        $(".userInfo__number").text(number__field1);
+        $(".userInfo__address").text(city__field1 + ", " + address__field1);
+
+        $("#popupForm").fadeOut();
+      } else {
+        alert("Please complete all fields.");
+      }
+    };
+  const showApplyCoupon = () =>
+    function () {
+      $(this).next(".cart__container_coupon--submit").slideToggle();
+    };
+  const showPopUp = () =>
+    function () {
+      $("#popupForm").fadeIn();
+    };
+  const showPopUpPay = () =>
+    function () {
+      $("#popUpPayment").fadeIn();
+    };
+  const closePopUp = () =>
+    function () {
+      $("#popupForm").fadeOut();
+    };
+  const closePopUpPay = () => {
+    $("#popUpPayment").fadeOut();
+  };
+
+  const handleChoosePayment = () => {
+    return function () {
+      const paymentMethod = $(this)
+        .closest(".popup--content--userInfo--action")
+        .find(".paymentMethod")
+        .text();
+      console.log(paymentMethod);
+      if (paymentMethod == "Google Pay") {
+        $("#popUpPayment").fadeOut();
+      } else if (paymentMethod == "Bank") {
+        $(".img__paymentMethod").attr(
+          "src",
+          "../../assets/icons/footer_trustbadge 1.svg"
+        );
+        $(".btn__pay--blue").text("Pay with Bank");
+        $("#popUpPayment").fadeOut();
+      }
+    };
+  };
+  const handlePayment = () =>
+    function () {
+      if ($(".btn__pay--blue").text() == "Pay with Bank") {
+        console.log("1");
+        $("#popUpPaymentQR").fadeIn();
+        handleCountDown();
+      }
+    };
+  const handleCountDown = () => {
+    let timeLeft = 300;
+
+    function formatTime(seconds) {
+      const minutes = Math.floor(seconds / 60);
+      const secondsLeft = seconds % 60;
+      return (
+        (minutes < 10 ? "0" : "") +
+        minutes +
+        ":" +
+        (secondsLeft < 10 ? "0" : "") +
+        secondsLeft
+      );
     }
-  });
 
-  // Close the popup if the user clicks outside of it
+    const countdownTimer = setInterval(function () {
+      timeLeft--;
+
+      $("#countdown").text(formatTime(timeLeft));
+
+      if (timeLeft <= 0) {
+        clearInterval(countdownTimer);
+        $("#countdown").text("Time's up!");
+        $("#popUpPaymentQR").fadeOut();
+      }
+    }, 1000);
+  };
+  $(".popup--content--submit").click(popUpUpdateInfo());
+  $(".cart__container--coupon").click(showApplyCoupon());
+  $("#openFormButton").click(showPopUp());
+  $("#closeFormButton").click(closePopUp());
+  $(".openFormButtonPay").click(showPopUpPay());
+  $("#closeFormButtonPay").click(closePopUpPay);
+  $(".popup--content--submitPayment").click(handleChoosePayment());
+  $(".btn__pay--blue").click(handlePayment());
   $(window).click(function (event) {
     if ($(event.target).is("#popupForm")) {
       $("#popupForm").fadeOut();
