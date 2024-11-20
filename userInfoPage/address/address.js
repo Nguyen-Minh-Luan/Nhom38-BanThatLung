27254183f0b597d111723ec0ffc343f29407dfe7
phanngochoa-22130083 <22130083@st.hcmuaf.edu.vn>
2024-11-20 20:59:51 +0700

    adding boostrap modal

--- userInfoPage/address/address.js
+++ userInfoPage/address/address.js
@@ -1,7 +1,4 @@
 $(document).ready(function () {
-  $(".custom_delete_address").on("click", function () {
-    $(this).closest(".address__info").remove();
-  });
   $(".custom_default").on("click", function () {
     $(".address__info").css("border", "2px solid white");
     $(this).closest(".address__info").css("border", "2px solid black");
