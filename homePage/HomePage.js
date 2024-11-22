cc6fc7396463826f69dddf9e7966664f003b7c4e
phanngochoa-22130083 <22130083@st.hcmuaf.edu.vn>
2024-11-22 18:26:15 +0700

    good image

--- homePage/HomePage.js
+++ homePage/HomePage.js
@@ -24,4 +24,3 @@ $(document).ready(function () {
     $(".custom_show").addClass("toast ");
   })
 });
-
--- menPage/menPage.js
+++ menPage/menPage.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
-    $(".filter__title").on("click", function () {
-        $("#dimmer3").toggleClass("active");
-        $("#overlay3").toggleClass("active");
-      });
-    
-      $(document).on("click", function (event) {
-        if (!$(event.target).closest("#overlay, #menuButton").length) {
-          $("#overlay").removeClass("active");
-          $("#dimmer").removeClass("active");
-        }
-      });
-      $("#closeButton").click(function () {
-        console.log(1);
-        $("#overlay3").removeClass("active");
-        $("#dimmer3").removeClass("active");
-      });
+  $(".filter__title").on("click", function () {
+      $("#dimmer3").toggleClass("active");
+      $("#overlay3").toggleClass("active");
+    });
+  
+    $(document).on("click", function (event) {
+      if (!$(event.target).closest("#overlay, #menuButton").length) {
+        $("#overlay").removeClass("active");
+        $("#dimmer").removeClass("active");
+      }
+    });
+    $("#closeButton").click(function () {
+      console.log(1);
+      $("#overlay3").removeClass("active");
+      $("#dimmer3").removeClass("active");
+    });
 })
