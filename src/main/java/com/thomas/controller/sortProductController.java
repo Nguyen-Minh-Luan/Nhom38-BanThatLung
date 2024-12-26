a46fda0cc133cbac90e40fa334606adc9757f60a
NguyenMinhLuan <22130154@st.hcmuaf.edu.vn>
2024-12-26 16:54:39 +0700

    add geDescendByPrice()

--- src/main/java/com/thomas/controller/sortProductController.java
+++ src/main/java/com/thomas/controller/sortProductController.java
@@ -1,20 +1,27 @@
 package com.thomas.controller;
 
+import com.thomas.dao.ProductDao;
+import com.thomas.dao.model.belts;
+import com.thomas.services.UploadProductService;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "sortProductController", value = "/sort")
 public class sortProductController extends HttpServlet {
-
+    UploadProductService uploadProductService = new UploadProductService();
+    ProductDao productDao = new ProductDao();
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
         String sort = request.getParameter("desPrice");
         out.println(sort);
+
     }
 
     @Override
--- src/main/java/com/thomas/dao/ProductDao.java
+++ src/main/java/com/thomas/dao/ProductDao.java
@@ -336,5 +336,28 @@ public class ProductDao {
             return handle.createQuery(sql).mapToBean(belts.class).list();
         });
     }
-
+    public List<belts> getAscendByPrice(){
+        return JDBIConnect.get().withHandle(handle -> {
+            String sql = "SELECT b.id,b.name,b.price,i.imagePath " +
+                    "FROM belts b " +
+                    "INNER JOIN imageEntry i " +
+                    "ON b.id = i.beltId " +
+                    "WHERE b.isDeleted = 0 " +
+                    "ORDER BY b.price " +
+                    "ASC";
+            return handle.createQuery(sql).mapToBean(belts.class).list();
+        });
+    }
+    public List<belts> getDescendByPrice(){
+        return JDBIConnect.get().withHandle(handle -> {
+            String sql = "SELECT b.id,b.name,b.price,i.imagePath " +
+                    "FROM belts b " +
+                    "INNER JOIN imageEntry i " +
+                    "ON b.id = i.beltId " +
+                    "WHERE b.isDeleted = 0 " +
+                    "ORDER BY b.price " +
+                    "DESC";
+            return handle.createQuery(sql).mapToBean(belts.class).list();
+        });
+    }
 }
